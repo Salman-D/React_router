7fb9161f9fe79b4aa47fe026ceae7f58ceda94a6
Salman-D <87569242+Salman-D@users.noreply.github.com>
2022-01-22 13:29:33 +0500

    again update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import Navigation from './config/Navigation';
-import { Route, Routes } from "react-router-dom";
-import About from "../components/About";
+
 
  function App() {
   return (
     <div className='App'>
        <Navigation/>
-
-       <Routes>
-         
-            <Route path="/about"   element={<About/>} /> 
-            
-        </Routes>
+     
     </div>
   )
 }
